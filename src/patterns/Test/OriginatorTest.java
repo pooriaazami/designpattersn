433f6ebd9d5757310ec446c56f4124b59185c5f8
Pooria Azami <pooriaazami@gmail.com>
2023-03-22 03:16:27 +0430

    test file added

---
+++ src/patterns/Test/OriginatorTest.java
@@ -0,0 +1,14 @@
+package patterns.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OriginatorTest {
+
+    @org.junit.jupiter.api.Test
+    void createState() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void restoreState() {
+    }
+}
